d22cfbf80196ee4eab5ae2ae814105ca47bc6d6f
ledeuhyang <jckim@org-i.com>
2023-12-14 16:05:46 +0900

    feat: properites git ignore

--- src/main/java/com/hawmock/global/cofig/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.hawmock.global.cofig;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
